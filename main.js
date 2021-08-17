37772784f17e8299412f187eacebdc562d491a8c
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-08-17 14:48:28 +0900

    add: .env

---
+++ main.js
@@ -0,0 +1,8 @@
+import dotenv from "https://taisukef.github.io/denolib/dotenv.js";
+import { MyServer } from "./src/server.js";
+
+// .envから環境変数を読込む
+dotenv.config();
+
+// サーバーを起動
+new MyServer(8881);
