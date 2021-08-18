b0f0f720b9594b26a2160df691fa25b2f06c38b8
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-08-18 10:41:30 +0900

    add: getPictures

--- src/server.js
+++ src/server.js
@@ -1,24 +1,15 @@
 import { Server } from "https://js.sabae.cc/Server.js";
 import { SearchHotels } from "./searchHotels.js";
-import { getPictureJson, getHashtag } from "./getPictureHashtag.js";
+import { getPictures } from "./getPicture.js";
 
 export class MyServer extends Server {
   api(path, req) {
     const to = path.replace(/\/api\//, "");
 
     switch (to) {
-      case "getPicture": {
-        const key = Deno.env.get("PIXABAY_KEY");
-        const url = `https://pixabay.com/api/?key=${key}&q=${encodeURIComponent(
-          req["key"]
-        )}`;
-        return getPictureJson(url);
-      }
-      case "getHashtag": {
-        const url = `https://instagram.userlocal.jp/hashtags/search?hashtags=${encodeURIComponent(
-          req["key"]
-        )}`;
-        return getHashtag(url);
+      case "getPictures": {
+        const results = getPictures(req.count);
+        return results;
       }
       case "searchHotels": {
         const results = SearchHotels(req.keyword, req.count);
