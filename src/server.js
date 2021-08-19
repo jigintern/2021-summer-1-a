49f00ce50f24dd27728d6babfb01b3fc8ce91401
Kazuryu0907 <41988990+Kazuryu0907@users.noreply.github.com>
2021-08-19 09:37:02 +0900

    server.jsにNearlyとHashを実装

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import { Server } from "https://js.sabae.cc/Server.js";
 import { SearchHotels } from "./searchHotels.js";
 import { getPictures } from "./getPicture.js";
+import { findNerlyPlace } from "./findNerlyPlace.js"
+import { getHashtag } from "./searchHashtag.js";
 
 export class MyServer extends Server {
   api(path, req) {
@@ -15,6 +17,17 @@ export class MyServer extends Server {
       // 人気の観光地の画像を取得
       case "getPictures": {
         const results = getPictures(req?.tags, req?.orderBy, req.count);
+        return results;
+      }
+      // 付近の観光地のurlを取得
+      case "findNerlyPlace": {
+        request = {categori:"all",lat:req.lat,lon:req.lon};
+        const results = findNerlyPlace(request,req.count);
+        return results;
+      }
+      //　関連したハッシュタグを取得
+      case "getHashtag": {
+        const results = getHashtag(req.keyword);
         return results;
       }
     }
