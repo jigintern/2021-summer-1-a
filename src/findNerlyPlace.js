00e4e139d5a7b8721bd0b7f2a560121b1c1a147d
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-08-19 10:19:52 +0900

    fix: 関数名

--- src/findNerlyPlace.js
+++ src/findNerlyPlace.js
@@ -1,25 +1,24 @@
-import { DOMParser, HTMLDocument } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 /**
  * 周辺の観光地を検索
  * @param {JSON} req {categori:"",lat:"",lon:""}lat->緯度
  * @param {int} step 取得する記事の数
  * @returns {Array} 取得したURLの配列
  */
-export async function getNearlyLocaiton(req,step){
+export async function findNerlyPlace(req, step) {
+  const url = `https://travel.navitime.com/ja/area/jp/interest/${req.categori}/?lat=${req.lat}&lon=${req.lon}`;
 
-    const url = `https://travel.navitime.com/ja/area/jp/interest/${req.categori}/?lat=${req.lat}&lon=${req.lon}`;
+  const res = await fetch(url);
+  const text = await res.text();
 
-    const res = await fetch(url);
-    const text = await res.text();
-    
-    const p = new DOMParser();
-    const doc = p.parseFromString(text,"text/html");
-    const urls = doc.querySelectorAll(".c-article-card>a");
-    const arr = [];
-    for(let i = 0;i<step;i++){
-        arr.push(urls[i].getAttribute('href'));
-    }
-    return arr;
+  const p = new DOMParser();
+  const doc = p.parseFromString(text, "text/html");
+  const urls = doc.querySelectorAll(".c-article-card>a");
+  const arr = [];
+  for (let i = 0; i < step; i++) {
+    arr.push(urls[i].getAttribute("href"));
+  }
+  return arr;
 }
 /*
 const lat = "43.059856";
