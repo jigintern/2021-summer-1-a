49fb2160d020740420158634aeb9652649286ffa
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-08-20 01:45:22 +0900

    update

--- src/sabae.js
+++ src/sabae.js
@@ -4,7 +4,7 @@ import { Geo3x3 } from 'https://taisukef.github.io/Geo3x3/Geo3x3.js';
 export function sabae() {
     return [
         {
-            title: '鯖江！',
+            title: '鯖江はここです！',
             pref: '鯖江市',
             author: '',
             place: 'https://upload.wikimedia.org/wikipedia/ja/d/d3/%E5%9F%BA%E7%A4%8E%E8%87%AA%E6%B2%BB%E4%BD%93%E4%BD%8D%E7%BD%AE%E5%9B%B3_18207.svg',
